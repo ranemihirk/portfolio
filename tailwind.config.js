267168b44edceab12382170eb3aba2e8a881f4cd
Mihir Rane <ranemihirk@gmail.com>
2023-04-21 01:50:10 +0530

    Changed css to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ["./src/app/**/*.{html,ts}",],
+  plugins: [require("daisyui")],
+  daisyui: {
+		darkTheme: 'light',
+	},
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors:{
+        'dark-color': 'hsl(210, 3%, 7%)',
+        'light-color': 'hsl(0, 0%, 98%)'
+      }
+    },
+  },
+}
