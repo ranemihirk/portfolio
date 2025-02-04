f69cecf1c37d6bf2300a70c88d6da982e4366771
Mihir Rane <ranemihirk@gmail.com>
2025-02-05 01:59:09 +0530

    wedding update

--- src/app/wedding/wedding.component.ts
+++ src/app/wedding/wedding.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { animate, scroll } from 'motion';
 
 @Component({
   selector: 'app-wedding',
@@ -8,14 +9,19 @@ import { Title } from '@angular/platform-browser';
   styleUrls: ['./wedding.component.css'],
 })
 export class WeddingComponent implements OnInit {
-  constructor(
-    private router: Router,
-    private titleService: Title,
-  ) {
+  constructor(private router: Router, private titleService: Title) {
     this.titleService.setTitle('Shivani - Mihir');
   }
 
   ngOnInit(): void {
-	
+    // Progress bar representing gallery scroll
+    scroll(animate('.progress', { scaleX: [0, 1] }, { ease: 'linear' }));
+
+    // document.querySelectorAll("section").forEach((section) => {
+    //   const header = section.querySelector("h2");
+    //   scroll(animate(header, { y: [-400, 400] }, { ease: "linear" }), {
+    //     target: header,
+    //   });
+    // });
   }
 }
